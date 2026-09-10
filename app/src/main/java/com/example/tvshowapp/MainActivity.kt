21e191a49b1882e113987b6caedf45376d1f9719
gitogit18 <anggitosulistyo@gmail.com>
2026-09-11 01:28:43 +0700

    Updated kotlinxCoroutinesTest version and cleaned up unused code in MainActivity

--- app/src/main/java/com/example/tvshowapp/MainActivity.kt
+++ app/src/main/java/com/example/tvshowapp/MainActivity.kt
@@ -4,15 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import com.example.tvshowapp.model.RetrofitInstance
-import com.example.tvshowapp.model.ShowRepository
 import com.example.tvshowapp.view.AppNavigation
-import com.example.tvshowapp.viewmodel.HomeViewModel
+
 
 class MainActivity : ComponentActivity() {
-
-    private val repository = ShowRepository(RetrofitInstance.api)
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)

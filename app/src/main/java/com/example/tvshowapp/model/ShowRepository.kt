d4af485f77d6e4e84185e898bdb404b45c0fefda
gitogit18 <anggitosulistyo@gmail.com>
2026-09-10 11:40:58 +0700

    feat: add TVMaze API and data layer

---
+++ app/src/main/java/com/example/tvshowapp/model/ShowRepository.kt
@@ -0,0 +1,13 @@
+package com.example.tvshowapp.model
+
+class ShowRepository(
+    private val api: TvMazeApi
+) {
+    suspend fun getShows(): list<Show> {
+        return api.getShows()
+    }
+
+    suspend fun getShow(id: Int): Show {
+        return api.getShow(id)
+    }
+}
